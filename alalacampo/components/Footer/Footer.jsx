b433687bfae74a8f04f2a21c129ce8b53ab012f2
Facupelli <facundopellicer4@gmail.com>
2022-05-30 12:57:31 -0300

    add login admin page, fix footer

--- alalacampo/components/Footer/Footer.jsx
+++ alalacampo/components/Footer/Footer.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import s from "./Footer.module.scss";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className={s.container}>
       <p>ALALA CAMPO</p>
+      <Link href="/login">
+        <p className={s.admin}>Admin</p>
+      </Link>
       <div>
         <a
           href="https://www.linkedin.com/in/facundo-pellicer-full-stack-developer/"
@@ -14,8 +18,8 @@ export default function Footer() {
           rel="noreferrer"
         >
           made with{" "}
-          <FontAwesomeIcon icon={faHeart} width="15px" height="15px" />{" "}
-          by Facundo Pellicer
+          <FontAwesomeIcon icon={faHeart} width="15px" height="15px" /> by
+          Facundo Pellicer
         </a>
       </div>
     </footer>
